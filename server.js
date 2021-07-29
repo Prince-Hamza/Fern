1fbc300b1abf050af2d95ca34ef231777722d60d
Prince-Hamza <princehamzi.mine@gmal.com>
2021-07-29 16:02:52 +0500

    good

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const firebaseRouter = require('./api/routes/firebaseRouter')
 const firebase = require('firebase')
 const firebaseConfig = require('./api/config')
 require('dotenv').config()
+console.log("cmon")
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
