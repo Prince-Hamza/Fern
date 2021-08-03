f9d6340dada3256726d144da4a7dd9dd524e78be
Prince-Hamza <princehamzi.mine@gmal.com>
2021-08-03 11:17:54 +0500

    ok

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ const firebaseRouter = require('./api/routes/firebaseRouter')
 const firebase = require('firebase')
 const firebaseConfig = require('./api/config')
 require('dotenv').config()
+
 console.log("startify")
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
 app.use(bodyParser.json())
-
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// });
 
 firebase.initializeApp(firebaseConfig)
 
@@ -25,6 +21,15 @@ app.use('/', express.static(__dirname + '/build'))
 const port = process.env.PORT;
 app.listen(port)
 console.log(`Server running on port: ${port}`)
+
+
+
+
+// app.use(function(req, res) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// });
 
 
 
